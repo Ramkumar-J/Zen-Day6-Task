1230b00bfdb6a3ee1286872254b61ac8f09f2ef3
ramkumar <ramviru9@gmail.com>
2022-08-06 20:53:26 +0530

    commit

--- loops/script.js
+++ loops/script.js
@@ -216,3 +216,25 @@ const myPrime=newArray.filter(num=>{
  return num===1;
 });
 console.log(myPrime);
+
+// Fix the code to sum the number in that array
+const num = [10, 20, 30, 40,50,60,70,80,90,100] 
+var sumofArr= (a, b) =>
+ a + b
+var sumofNum = num.reduce(sumofArr);
+console.log(sumofNum);
+
+// Fix the code to rotate an array by k times and return rotated array using IIFE function
+var array = [1, 2, 3, 6, 8, 6, 1, 9, 10, 12, 13];
+var k = 3;
+k = array.length % k;
+(function(arr) {
+ arr = {};
+ out = arr.slice(k + 1, arr.length);
+ var count = out.length;
+ for (var i = 0; i < k + 1; i++) {
+ out[count] = arr[i];
+ count += 1;
+ }
+ console.log(out);
+})(array);
